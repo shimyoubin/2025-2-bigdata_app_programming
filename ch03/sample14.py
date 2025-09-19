33bed1071c04dd419efc1ddbfc700e9269918185
214 <shimub05@naver.com>
2025-09-19 09:58:50 +0900

    수정1

--- ch03/sample14.py
+++ ch03/sample14.py
@@ -1,10 +1,9 @@
-from types import new_class
 
 my_book = 2005, 'pyton', 100, '2class'
 your_book = [2010, 'java', 200, '2class']
 
-new_book = your_book[:]
-new_book = your_book #
+#new_book = your_book[:]
+new_book = your_book # 얕은복사 -> 원본바뀌면 같이 바뀜
 
 your_book[0]=2025
 
